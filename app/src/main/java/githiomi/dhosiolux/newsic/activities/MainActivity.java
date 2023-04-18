1022d7089d34efa7157dc2590dfe740fbf1ed4d3
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-18 23:41:27 +0400

    Bound all views in main activity

--- app/src/main/java/githiomi/dhosiolux/newsic/activities/MainActivity.java
+++ app/src/main/java/githiomi/dhosiolux/newsic/activities/MainActivity.java
@@ -1,16 +1,31 @@
 package githiomi.dhosiolux.newsic.activities;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-import githiomi.dhosiolux.newsic.R;
+import githiomi.dhosiolux.newsic.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Layout Views
+    RecyclerView categoriesRV, articlesRV;
+    RelativeLayout trendingLoading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // View Binding set up
+        ActivityMainBinding mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Set Content view
+        setContentView(mainBinding.getRoot());
+
+        // Binding Views
+        categoriesRV = mainBinding.RVCategories;
+        articlesRV = mainBinding.RVTrendingNews;
+        trendingLoading = mainBinding.RLLoading;
     }
 }
