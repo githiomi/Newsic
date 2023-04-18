0ab7d2b14d060bcd77d29c105c90ac8e9d6cfaf3
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-18 22:51:39 +0400

    Created the category recycler view adapter

---
+++ app/src/main/java/githiomi/dhosiolux/newsic/adapters/CategoryAdapter.java
@@ -0,0 +1,74 @@
+package githiomi.dhosiolux.newsic.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import githiomi.dhosiolux.newsic.R;
+import githiomi.dhosiolux.newsic.models.Category;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+
+    // Adapter Variables
+    List<Category> categoriesList = new ArrayList<Category>();
+    Context context;
+
+    // Constructor
+    public CategoryAdapter(List<Category> categoriesList, Context context) {
+        this.categoriesList = categoriesList;
+        this.context = context;
+    }
+
+    // Adapter Methods
+    @NonNull
+    @Override
+    public CategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.rv_item_category, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.ViewHolder holder, int position) {
+
+        // Get the specific category at the given position
+        Category category = categoriesList.get(position);
+
+        // Populate the view
+        holder.categoryName.setText(category.getCategoryName());
+
+        Picasso.get().load(category.getCategoryImageUrl()).into(holder.categoryImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categoriesList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Layout views
+        TextView categoryName;
+        ImageView categoryImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Binding Views
+            categoryName = (TextView) itemView.findViewById(R.id.TV_categoryName);
+            categoryImage = (ImageView) itemView.findViewById(R.id.IV_categoryBG);
+
+        }
+    }
+}
