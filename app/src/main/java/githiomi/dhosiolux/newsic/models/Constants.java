babc36aa0500743126f9dfa166d080476223b880
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-20 11:00:15 +0400

    Added API key to constants file

--- app/src/main/java/githiomi/dhosiolux/newsic/models/Constants.java
+++ app/src/main/java/githiomi/dhosiolux/newsic/models/Constants.java
@@ -15,8 +15,8 @@ public class Constants {
 
     public static final String SORT_BY = "publishedAt";
 
-//    public static final String NEWS_API_KEY = BuildConfig.NEWS_API_APIKEY;
-public static final String NEWS_API_KEY = "e56d55726f354d99bbaff93c5e374f5b";
+    //    public static final String NEWS_API_KEY = BuildConfig.NEWS_API_APIKEY;
+    public static final String NEWS_API_KEY = "e56d55726f354d99bbaff93c5e374f5b";
 
     public static final String GET_ALL_NEWS_URL = "https://newsapi.org/v2/top-headlines?country=" +
             COUNTRY + "&excludeDomains=" + EXCLUDED_DOMAIN + "&sortBy=" + SORT_BY + "&language=" +
