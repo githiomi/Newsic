813efb32ac3afc86d8835efa94ea9dede394f055
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-20 18:51:19 +0400

    Added splash screen delay in the activity

--- app/src/main/java/githiomi/dhosiolux/newsic/activities/SplashScreenActivity.java
+++ app/src/main/java/githiomi/dhosiolux/newsic/activities/SplashScreenActivity.java
@@ -1,7 +1,9 @@
 package githiomi.dhosiolux.newsic.activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +16,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        // Keep splash screen open for 4 seconds
+        new Handler().postDelayed(() -> {
+
+            startActivity(new Intent(this, MainActivity.class));
+            finish(); // To the destroy activity
+
+        }, 4000);
     }
 }
